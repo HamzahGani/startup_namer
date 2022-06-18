7fe532c70137cb3f72aa3203d16c8c8501b61dde
HGani <hgani786@gmail.com>
2022-06-18 17:37:43 +0200

    view saved name on new page

--- lib/main.dart
+++ lib/main.dart
@@ -6,15 +6,20 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Startup Name Generator',
-      home: Scaffold(
-        appBar: AppBar( title: const Text('Startup Name Generator'), ),
-        body: const Center( child: RandomWords(), ),
-      ), );
+      theme: ThemeData(
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: RandomWords(),
+    );
   }
 }
 
@@ -29,40 +34,82 @@ class _RandomWordsState extends State<RandomWords> {
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (context) {
+          final tiles = _saved.map(
+              (pair) {
+                return ListTile(
+                  title: Text(
+                    pair.asPascalCase,
+                    style: _biggerFont,
+                  ),
+                );
+              },
+          );
+          final divided = tiles.isNotEmpty
+            ? ListTile.divideTiles(
+                context: context,
+                tiles: tiles,
+              ).toList()
+              : <Widget>[];
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     //return Text(WordPair.random().asPascalCase);
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Startup Name Generator'),
+        actions: [ IconButton(
+          icon: const Icon(Icons.list),
+          onPressed: _pushSaved,
+          tooltip: 'Saved Suggestions',
+        ),]
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(16.0),
+          itemBuilder: (context, i) {
+            if (i.isOdd) return const Divider();
 
-          final index = i ~/2;
-          if (index >= _suggestions.length) _suggestions.addAll(generateWordPairs().take(10));
+            final index = i ~/2;
+            if (index >= _suggestions.length) _suggestions.addAll(generateWordPairs().take(10));
 
-          final alreadySaved = _saved.contains(_suggestions[index]);
+            final alreadySaved = _saved.contains(_suggestions[index]);
 
-          //return Text(_suggestions[index].asPascalCase);
-          return ListTile( title: Text(
+            //return Text(_suggestions[index].asPascalCase);
+            return ListTile( title: Text(
               _suggestions[index].asPascalCase,
               style: _biggerFont,
             ),
-            trailing: Icon(
-              alreadySaved? Icons.favorite : Icons.favorite_border,
-              color: alreadySaved? Colors.red : null,
-              semanticLabel: alreadySaved? 'Remove from saved' : 'saved',
-            ),
-            onTap: () {
-              setState(() {
-                if(alreadySaved){
-                  _saved.remove(_suggestions[index]);
-                } else {
-                  _saved.add(_suggestions[index]);
-                }
-              });
-            },
-          );
-        }
+              trailing: Icon(
+                alreadySaved? Icons.favorite : Icons.favorite_border,
+                color: alreadySaved? Colors.red : null,
+                semanticLabel: alreadySaved? 'Remove from saved' : 'saved',
+              ),
+              onTap: () {
+                setState(() {
+                  if(alreadySaved){
+                    _saved.remove(_suggestions[index]);
+                  } else {
+                    _saved.add(_suggestions[index]);
+                  }
+                });
+              },
+            );
+          }
+      )
     );
   }
 }
