46dcea929ea957e5ff35148b22b76ce86cbf5688
HGani <hamzah@katlegosolutions.co.za>
2022-06-18 15:21:31 +0200

    add heart icon

--- lib/main.dart
+++ lib/main.dart
@@ -10,43 +10,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Startup Name Generator',
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutterring'),
-        ),
-        body: const Center(
-          child: RandomWords(),
-        ),
-      ),
-    );
+        appBar: AppBar( title: const Text('Startup Name Generator'), ),
+        body: const Center( child: RandomWords(), ),
+      ), );
   }
 }
 
 class RandomWords extends StatefulWidget {
   const RandomWords({Key? key}) : super(key: key);
-
   @override
   State<RandomWords> createState() => _RandomWordsState();
 }
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
-  //final _biggerFont = const TextStyle(fontSize: 18);
+  final _saved = <WordPair>{};
+  final _biggerFont = const TextStyle(fontSize: 18);
+
   @override
   Widget build(BuildContext context) {
-    //final wordPair = WordPair.random();
-    //return Text(wordPair.asPascalCase);
+    //return Text(WordPair.random().asPascalCase);
     return ListView.builder(
         padding: const EdgeInsets.all(16.0),
         itemBuilder: (context, i) {
           if (i.isOdd) return const Divider();
 
           final index = i ~/2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return Text(_suggestions[index].asPascalCase);
+          if (index >= _suggestions.length) _suggestions.addAll(generateWordPairs().take(10));
+
+          final alreadySaved = _saved.contains(_suggestions[index]);
+
+          //return Text(_suggestions[index].asPascalCase);
+          return ListTile( title: Text(
+              _suggestions[index].asPascalCase,
+              style: _biggerFont,
+            ),
+            trailing: Icon(
+              alreadySaved? Icons.favorite : Icons.favorite_border,
+              color: alreadySaved? Colors.red : null,
+              semanticLabel: alreadySaved? 'Remove from saved' : 'saved',
+            ),
+            onTap: () {
+              setState(() {
+                if(alreadySaved){
+                  _saved.remove(_suggestions[index]);
+                } else {
+                  _saved.add(_suggestions[index]);
+                }
+              });
+            },
+          );
         }
     );
   }
